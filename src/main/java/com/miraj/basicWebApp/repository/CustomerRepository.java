639c05727b9175377d0b68ccb6e5bae5e4c0e47b
Miraj Hamid <miraj.hamid@axiatadigitallabs.com>
2019-09-18 14:45:16 +0530

    update repository

--- src/main/java/com/miraj/basicWebApp/repository/CustomerRepository.java
+++ src/main/java/com/miraj/basicWebApp/repository/CustomerRepository.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import com.miraj.basicWebApp.model.Customer;
 
-//The Model and it's id datatype
+/*The Model and it's id datatype
+better if you can use JPARepository because it has both and JPA functionalities 
+JpaRepository extends PagingAndSortingRepository which in turn extends CrudRepository.
+If you use JpaRepository annotate package in Applicationclass
+@EnableJpaRepositories (basePackages = 'repository.package.name')*/
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
 	
 	public Customer findById(int id);
